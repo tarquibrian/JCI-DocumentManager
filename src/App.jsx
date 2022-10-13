bab08ee5e3f246c0cf550ab58c6c81bab6921eff
tarquibrian <tarquibrian@gmail.com>
2022-10-13 23:31:00 +0200

    initial version jci-document manager

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// CSS FILES
+import "./styles/globalStyles.css";
+
+// PAGES
+
+const App = () => {
+  return (
+    <>
+      <h1 className="container">APP</h1>
+    </>
+  );
+};
+
+export default App;
